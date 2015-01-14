863fd5049921c3f07d39e4c3fb0e0aced22ff0ad
nathan-p <nathan.prior.31@gmail.com>
2015-01-14 18:26:21 +0100

    tets

--- MarkingMenu/src/markingmenu/PopButton.java
+++ MarkingMenu/src/markingmenu/PopButton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package popbutton;
+package markingmenu;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
--- MarkingMenu/src/markingmenu/TicTacToeItem.java
+++ MarkingMenu/src/markingmenu/TicTacToeItem.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package popbutton;
+package markingmenu;
 
 import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
